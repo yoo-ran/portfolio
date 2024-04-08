86fb3248281eb6d789990dacf0c8a06dfd106895
YooranKim <yuranm@naver.com>
2024-04-07 21:17:55 -0700

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faChevronRight} from '@fortawesome/free-solid-svg-icons';
